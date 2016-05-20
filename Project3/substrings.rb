d8cb938f67f340f9f85ef864229c367d460ede79
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-05-20 08:42:44 -0700

    Started Project 3

---
+++ Project3/substrings.rb
@@ -0,0 +1,5 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts "Text please"
+text = gets.chomp
+words = text.downcase.split(" ")
